aaa29de00bd7070a6f2e761864cf3dd2b24e92e1
IsraelR1099 <rifarachi10@gmail.com>
2024-09-19 20:59:27 +0200

    smaller buf in level1

--- level1/source.c
+++ level1/source.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
 int	main(void)
 {
-	char	buf[100] = {0};
+	char	buf[20] = {0};
 	char	*s1 = "Good job.\n";
 	char	*s2 = "Nope.\n";
 	char	*password = "__stack_check";
 
 	printf("Please enter key: ");
-	scanf("%s", buf);
+	scanf("%19s", buf);
 	if (strcmp(buf, password) == 0)
 	{
 		printf("%s", s1);
 		return (0);
 	}
 	printf("%s", s2);
-	return EXIT_SUCCESS;
+	return (0);
 }
